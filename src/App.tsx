84eb7b3237ac074ada0e46648e8e6a9c475f8072
sammuttai <muttaisammy@gmail.com>
2023-03-15 11:10:53 +0300

    DAP 7 - Add navbar and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header/Header";
+import SideNavBar from "./components/SideNavBar/SideNavBar";
+
 const App =  () => {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center"> Hello World! </h1>
+    <Header/>
+    <SideNavBar />
     </>
   );
 }
