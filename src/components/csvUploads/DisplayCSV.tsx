b2a7f6771167946e9e0ba1ad48dbb2ba364cb791
MuttaiSam <muttaisammy@gmail.com>
2023-03-24 11:42:16 +0300

    DAP -17

--- src/components/csvUploads/DisplayCSV.tsx
+++ src/components/csvUploads/DisplayCSV.tsx
@@ -1,43 +1,100 @@
+
 import React from 'react'
+import { useState, useEffect } from "react";
+import Pagination from "../patientSearch/Pagination";
+import {getCsvFiles} from './csv.resource';
+//import { getCsvFiles } from "./csv.resource";
 
 const DisplayCSV = () => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [dataPerPage] = useState<number>(5);
+  const indexOfLastPatient = currentPage * dataPerPage;
+  const indexOfFirstPatients = indexOfLastPatient - dataPerPage;
+  const currentData = data.slice(
+    indexOfFirstPatients,
+    indexOfLastPatient
+  );
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  useEffect(() => {
+    const fetchCsvFiles = async () => {
+      const res = await getCsvFiles();
+      const response = await res.json();
+      setData(response);
+    };
+    fetchCsvFiles();
+  }, []);
+
+
+
+
+  
   return (
-    <div className="w-[60%] mx-auto mt-[18%]">
-        <div>
-        <div className="overflow-x-auto shadow-md">
-            <table className="w-full text-sm text-left text-gray-500 ">
-              <thead className="text-xs text-gray-700 uppercase bg-blue-300">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                   Username
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Collection Date
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Type
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
+    <div className="w-[60%] mx-auto mt-[17%]">
+      <div>
+      {currentData.length > 0 && (
+       <>
+        <span className="text-xl font-bold pb-4">Total Records: {data.length}</span>
+          <div className="overflow-x-auto shadow-md">
+          <table className="w-full text-sm text-left text-gray-500 ">
+            <thead className="text-xs text-gray-700 uppercase bg-blue-300">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  File Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Uploaded time
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Type
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentData.map((item: any) => (
+                <>
                   <tr
-                    className="bg-white border-b hover:cursor-pointer"
+                    className="bg-white border-b"
+                    key={item.id}
                   >
-
-                    <td className="px-6 py-4">John doe</td>
-                    <td className="px-6 py-4">01/01/2023</td>
-                    <td className="px-6 py-4"><span className='bg-lime-500 p-2'>VL</span></td>
-                    <td className="px-6 py-4">SYNCED</td>
+                    <td className="px-6 py-4">{item.file_name}</td>
+                    <td className="px-6 py-4">{item.upload_time}</td>
+                    <td className="px-6 py-4">{item.status}</td>
+                    <td className="px-6 py-4">
+                      <span
+                        className={`${
+                          item.file_type == "VL"
+                            ? "bg-green-500 py-2 px-3 text-white"
+                            : "bg-btnColor p-2 text-white"
+                        }`}
+                      >
+                        {item.file_type}
+                      </span>
+                    </td>
+                    <td className="px-4 py-2 "><button className="bg-blue-400 p-2 rounded-md cursor-pointer text-white">SYNC</button></td>
                   </tr>
-              </tbody>
-            </table>
-          </div>
+                </>
+              ))}
+            </tbody>
+          </table>
         </div>
+       </>
+      )}
+     {dataPerPage && (
+                      <Pagination
+                        patientsPerPage={dataPerPage}
+                        totalPatients={data.length}
+                        paginate={paginate}
+                      />
+                    )}
+      </div>
     </div>
-  )
-}
-
-export default DisplayCSV
+  );
+};
+export default DisplayCSV;
--- src/components/csvUploads/csv.resource.ts
+++ src/components/csvUploads/csv.resource.ts
@@ -1,22 +1,9 @@
-export interface CsvUploadData {
-    file: any,
-    username: string,
-    file_type: string
-}
 
-const url = 'http://localhost:3000/api/csv/uploads'
+const url = 'http://localhost:3005/api/csv/uploads'
 
-export const uploadCsvFile = async (data: CsvUploadData) => {
-    const formData = new FormData();
 
-    formData.append('file', data.file)
-    formData.append('username', data.username)
-    formData.append('file_type', data.file_type)
 
-    const response = await fetch(url, {
-        method: 'POST',
-        body: formData,
-    });
+export const getCsvFiles = async () => {
+    const response = await fetch(url);
     return response
-
 }
